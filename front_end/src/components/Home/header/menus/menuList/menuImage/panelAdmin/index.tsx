9d9ab119f6e43d109d8e98baa9223dff3f38077d
amir <amirtaki623@gmail.com>
2026-04-21 00:12:43 +0330

    image slice at projects

---
+++ front_end/src/components/Home/header/menus/menuList/menuImage/panelAdmin/index.tsx
@@ -0,0 +1,90 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import type { AppDispatch, RooState } from "../../../../../../../store";
+import { useEffect } from "react";
+import { changeStatusItemListHeadersThunk, deleteItemListHeadersThunk, viewListHeadersThunk } from "../redux/actionsMenuList";
+
+const MenuImageHeadersPA = () => {
+    // const {Lists, loading} = useSelector((state: RooState) => state.lists)
+   
+    const dispatch = useDispatch<AppDispatch>()
+    useEffect(() => {
+        // dispatch(viewListHeadersThunk())
+    }, [loading]) 
+    return(
+        <div className={`flex w-full flex-col gap-5`}>
+            
+            <Link 
+                className="text-center text-4xl my-3 cursor-pointer hover:text-sky-500 duration-200 hover:tracking-[.3rem]"
+                to = "create" 
+            >
+                create list item
+            </Link>
+            
+            <div className="w-[90%] mx-auto flex flex-col my-3 overflow-x-auto " >
+                
+                <table className="w-full">
+                    <thead >
+                        <tr >
+                            <th>#</th>
+                                <th >Image</th>
+                            <th>List</th>
+                            <th>Title</th>
+                            <th>Caption</th>
+                            <th>status</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody >
+                        {/* {Array.isArray(Lists) && Lists.map((item, ind) => { 
+                            return(
+                                <tr key = {ind} className=" " >
+                                    
+                                    <th className="">{ind + 1}</th>
+                                    
+                                    <th></th>
+                                                            
+                                    <th>{item.list}</th>
+                                    <th>{item.title}</th>
+
+                                    <th className = {`${item.status == 10 ? "text-green-400" : 'text-rose-400'  } `}>
+                                        {item.status == 10 ? 'enable' : 'disable'}
+                                    </th>
+
+                                    <th>
+                                        <div 
+                                            onClick={() => {
+                                                dispatch(changeStatusItemListHeadersThunk({id: item.id}) );
+                                            }}
+                                            className="text-yellow-500 cursor-pointer duration-200 hover:text-yellow-300 py-1 "
+                                        >
+                                            change status
+                                        </div>
+
+                                        <Link  
+                                            to = {`edit/${item.id}`} 
+                                            className="text-sky-500 cursor-pointer duration-200 hover:text-blue-700 py-1">
+                                                edit
+                                        </Link>
+                                        
+                                        <div 
+                                            onClick={() => {
+                                               window.confirm('Do you want it to be deleted ? ')  && dispatch(deleteItemListHeadersThunk({id: item.id}))                                            
+                                            }}
+                                            className="text-rose-500 cursor-pointer duration-200 hover:text-red-700! py-1"
+                                        >
+                                            delete
+                                        </div>
+                                    </th>
+                                </tr>
+                            )
+                        })} */}
+
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
+    )
+}
+export default MenuImageHeadersPA
---
+++ front_end/src/components/Home/header/menus/menuList/menuImage/redux/sliceImageMenus.ts
@@ -0,0 +1,85 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export type TImageMenuHeader =  Array<{id: number, image: string,  list: string, title: string, body: string, status: number, created_at: string, updated_at: string }> | boolean  | string
+export type TImageMenuHeaderObject = {id: number, image: string,  list: string, title: string, body: string, status: number, created_at: string, updated_at: string }
+
+interface IImage{
+    images: TImageMenuHeader | [],
+    warningMessage: string,
+
+    // cereate & edit 
+    urlImage: string , // save image url
+    image: {name: string ,warning: string},
+    body: {name: string, warning: string},
+    title: {name: string, warning: string},
+    list: {name: string, warning: string},
+    callback: boolean, 
+    addItems: boolean,
+
+    // delete & status
+    loading: boolean
+}
+
+const initialState: IImage = {
+    images:  [],
+    warningMessage: '',
+
+    // cereate & edit 
+    urlImage: '' , // save image url
+   
+    image: {name: '' ,warning: ''},
+    body:  {name: '' ,warning: ''},
+    title: {name: '' ,warning: ''},
+    list:  {name: '' ,warning: ''},
+   
+    callback: false, 
+    addItems: false,
+
+    // delete & status
+    loading: false
+}
+
+const imageMenuSlice = createSlice({
+    name: 'image_menus_slice_toolkit',
+    initialState: initialState, 
+    reducers: {
+        onSetURLImage: (state, action) => {
+            state.urlImage = action.payload.result
+        },
+        onImageMenus: (state, action) => {
+            state.image = {name: action.payload.image, warning: ''}
+        },
+        onBodyImage: (state, action) => {
+            state.body = {name: action.payload.body, warning: ''}
+        }, 
+        onTitleImage: (state, action) =>  {
+            state.title = {name: action.payload.title, warning: ''}
+        },
+        onListImage: (state, action) => {
+            state.list = {name: action.payload.list, warning: ''}
+        },
+        onWarningImage: (state, action) => {
+            state.image = {name: state.image.name, warning: action.payload.image}
+            state.body = {name: state.body.name, warning: action.payload.body},
+            state.title = {name: state.title.name, warning: action.payload.title},
+            state.list = {name: state.list.name, warning: action.payload.list}
+        },
+        onCallBackImage: (state) => {
+            state.callback = true
+        },
+        onAddItemsImage: (state) => {
+            state.addItems = false
+        },
+        onLoadingImage: (state) => {
+            state.image = {name: '', warning: ""}
+            state.title = {name: '', warning: ""}
+            state.body = {name: '', warning: ""}
+            state.list = {name: '', warning: ""}
+        }
+
+    },
+    extraReducers: (builder) => {}
+})
+
+export default imageMenuSlice
+export const {onBodyImage, onCallBackImage ,onImageMenus, onListImage, onTitleImage, onLoadingImage, onSetURLImage, onWarningImage, onAddItemsImage} = imageMenuSlice.actions
